5321810c7bdabd09f65b8deb813a86191c2f4528
serog001 <serog001@stud.fh-dortmund.de>
2020-08-27 09:55:40 +0200

    added chart.zul

--- getZkUp/src/main/java/tutorial/SearchViewModel.java
+++ getZkUp/src/main/java/tutorial/SearchViewModel.java
@@ -1,6 +1,8 @@
 package tutorial;
 
+import java.lang.reflect.Array;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 
 import org.zkoss.bind.annotation.*;
@@ -8,6 +10,7 @@ import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.event.Event;
 import org.zkoss.zk.ui.select.SelectorComposer;
 import org.zkoss.zk.ui.select.annotation.Listen;
+import org.zkoss.zk.ui.util.Clients;
 import org.zkoss.zul.ListModelList;
 
 public class SearchViewModel {
@@ -31,6 +34,8 @@ public class SearchViewModel {
         fragebogensList.addAll(carService.searchFragebogen(keyword));
 
 
+        this.schowChart("linie", null);
+
         items.addAll(fragebogensList);
     }
 
@@ -46,6 +51,11 @@ public class SearchViewModel {
         System.out.println("Hallo!");
     }
 
+
+    private void schowChart(String type, HashMap<String, Object[]> params){
+
+        Clients.evalJavaScript(this.getChartCode());
+    }
 
     public void setKeyword(String keyword) {
         this.keyword = keyword;
@@ -81,5 +91,29 @@ public class SearchViewModel {
 
     public void setMessage(String message) {
         this.message = message;
+    }
+
+    private String getChartCode(){
+        return "google.charts.load('current', {'packages':['corechart']});\n" +
+                "      google.charts.setOnLoadCallback(drawChart);\n" +
+                "\n" +
+                "      function drawChart() {\n" +
+                "        var data = google.visualization.arrayToDataTable([\n" +
+                "          ['Year', 'Sales', 'Expenses'],\n" +
+                "          ['2013',  1000,      400],\n" +
+                "          ['2014',  1170,      460],\n" +
+                "          ['2015',  660,       1120],\n" +
+                "          ['2016',  1030,      540]\n" +
+                "        ]);\n" +
+                "\n" +
+                "        var options = {\n" +
+                "          title: 'Company Performance',\n" +
+                "          hAxis: {title: 'Year',  titleTextStyle: {color: '#333'}},\n" +
+                "          vAxis: {minValue: 0}\n" +
+                "        };\n" +
+                "\n" +
+                "        var chart = new google.visualization.AreaChart(document.getElementsByTagName('body')[0].children[0].children[0]);\n" +
+                "        chart.draw(data, options);\n" +
+                "      }";
     }
 }
